98cc17cbc4b3ed3f1661b072fd8c1ce39f4e4a12
Jon Scheiding <jonscheiding@gmail.com>
2017-03-22 17:06:51 -0400

    Added NPM util to generate the PWM lookup table.

---
+++ pwm-lookup-generator/main.js
@@ -0,0 +1,34 @@
+const pad = require('pad');
+const splitArray = require('split-array');
+
+const MICROSECONDS_PER_SECOND = 1000000;
+const CONFIG = require('./config.json');
+
+const periodInMicroseconds = (1 / CONFIG.frequencyInHz) * MICROSECONDS_PER_SECOND;
+const steps = (periodInMicroseconds / CONFIG.resolutionInMicroseconds);
+const f = x => Math.sin(2 * Math.PI * x);
+
+const formatArray = (array, lines, linePrefix) => {
+  const itemsPerLine = array.length / lines;
+  const lineChunks = splitArray(result, 25);
+  
+  formatLine = lineChunk => lineChunk.map(item => pad(4, item.toString())).join(',');
+  return lineChunks.map(lineChunk => linePrefix + formatLine(lineChunk)).join(',\n');
+};
+
+const result = [];
+
+for(let step = 0; step < (steps / 2); step++) {
+  const time = (1 / steps) * step;
+  const value = CONFIG.amplitude * f(time);
+  
+  result.push(Math.round(value));
+}
+
+console.log(`
+const short PWM_LOOKUP[] = {
+${formatArray(result, 10, '    ')}
+};
+const int PWM_LOOKUP_LENGTH = ${result.length};
+const int PWM_STEP_MICROSECONDS = ${CONFIG.resolutionInMicroseconds};
+`);
